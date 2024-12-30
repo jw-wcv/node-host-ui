0ffc42dd9b1021cd289714515b906bc21a091c3f
jw-wcv <101585096+jw-wcv@users.noreply.github.com>
2024-12-29 16:56:27 -0800

    Update data.js

--- src/resources/data.js
+++ src/resources/data.js
@@ -1,5 +1,5 @@
 // Pricing tiers for VMs
-const VM_TIERS = [
+export const VM_TIERS = [
     { cores: 1, ram: 2, cost: 2000 },
     { cores: 2, ram: 4, cost: 4000 },
     { cores: 4, ram: 8, cost: 8000 },
